610671db89e24d4714cc485e14003fc487a68ba0
Philippe Chretien <philippe.chretien@gmail.com>
2014-10-29 12:38:26 -0400

    Working environment with both Jersey and GAE

--- src/main/java/com/basbrun/gae/jersey/GaeTemplateResource.java
+++ src/main/java/com/basbrun/gae/jersey/GaeTemplateResource.java
@@ -1,5 +1,5 @@
 
-package com.basbrun.gae;
+package com.basbrun.gae.jersey;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -7,8 +7,8 @@ import javax.ws.rs.Produces;
 
 /** Example resource class hosted at the URI path "/myresource"
  */
-@Path("/myresource")
-public class MyResource {
+@Path("/gaetemplate")
+public class GaeTemplateResource {
     
     /** Method processing HTTP GET requests, producing "text/plain" MIME media
      * type.
